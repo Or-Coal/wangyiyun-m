61079635590a7ff04abf9035632a3760a3ae956f
Or-Coal <975015584@qq.com>
2022-08-26 23:41:40 +0800

    引入阿里字体图标库

--- wangyiyun/src/views/HomeView.vue
+++ wangyiyun/src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <svg class="icon" aria-hidden="true">
+      <use xlink:href="#tou-tiaogengduo"></use>
+    </svg>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
